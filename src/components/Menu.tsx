88155a49335a7770b9f5ab3c5b339e294874b9bb
Monty Lennie <montylennie@gmail.com>
2021-07-29 19:18:09 -0700

    add privacy, cookie and terms links to menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -13,7 +13,7 @@ import {
 
 import { Storage } from "@capacitor/storage";
 import { useLocation } from 'react-router-dom';
-import { logInOutline,
+import { logInOutline, eyeOutline, speedometerOutline, newspaperOutline,
          personAddOutline
        } from 'ionicons/icons';
 import './Menu.css';
@@ -23,6 +23,7 @@ interface AppPage {
   url: string;
   iosIcon: string;
   mdIcon: string;
+  targetBlank: boolean;
   title: string;
 }
 
@@ -30,14 +31,37 @@ const appPages: AppPage[] = [
   {
     title: 'Register',
     url: '/register',
+    targetBlank: false,
     iosIcon: personAddOutline,
     mdIcon: personAddOutline
   },
   {
     title: 'Login',
     url: '/login',
+    targetBlank: false,
     iosIcon: logInOutline,
     mdIcon: logInOutline
+  },
+  {
+    title: 'Privacy Policy',
+    url: 'https://www.iubenda.com/privacy-policy/25768068/full-legal',
+    targetBlank: true,
+    iosIcon: eyeOutline,
+    mdIcon: eyeOutline
+  },
+  {
+    title: 'Cookie Policy',
+    url: 'https://www.iubenda.com/privacy-policy/25768068/cookie-policy',
+    targetBlank: true,
+    iosIcon: speedometerOutline,
+    mdIcon: speedometerOutline
+  },
+  {
+    title: 'Terms and Conditions',
+    url: 'https://www.iubenda.com/terms-and-conditions/25768068',
+    targetBlank: true,
+    iosIcon: newspaperOutline,
+    mdIcon: newspaperOutline
   },
 ];
 
@@ -60,6 +84,7 @@ const Menu: React.FC = () => {
   }, []);
 
 
+  let targetBlankReached = false;
   return (
     <IonMenu side="start" contentId="main" type="overlay">
       <IonContent>
@@ -71,12 +96,19 @@ const Menu: React.FC = () => {
             ? appPages.map((appPage, index) => {
               return (
                 <IonMenuToggle key={index} autoHide={false}>
-                  <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  { appPage.iosIcon && appPage.mdIcon &&
-                    <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                  { appPage.targetBlank ?
+                    <IonItem href={appPage.url} target="_blank" routerDirection="none" lines="none" detail={false}>
+                      <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                      <IonLabel>{appPage.title}</IonLabel>
+                    </IonItem>
+                    :
+                    <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                      <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                      <IonLabel>{appPage.title}</IonLabel>
+                    </IonItem>
+
                   }
-                    <IonLabel>{appPage.title}</IonLabel>
-                  </IonItem>
+
                 </IonMenuToggle>
               );
             })
