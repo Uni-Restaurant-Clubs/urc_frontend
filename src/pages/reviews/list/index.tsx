0d7a29822c126c57de1d34f693b5a95749845dd2
Monty Lennie <montylennie@gmail.com>
2021-08-11 20:21:01 -0700

    get reviews showing in reviews page

--- src/pages/reviews/list/index.tsx
+++ src/pages/reviews/list/index.tsx
@@ -3,6 +3,10 @@ import { reviewActions } from "../../../redux/actions/reviewActions";
 import { useDispatch, useSelector } from "react-redux";
 import {
   IonButton,
+  IonThumbnail,
+  IonImg,
+  IonList,
+  IonItem,
   IonContent,
   IonCard,
   IonHeader,
@@ -15,10 +19,21 @@ import { Link } from "react-router-dom";
 import Header from "../../../components/Header";
 import "./index.css"
 
-
-const ReviewPage: React.FC = () => {
+const ReviewsPage: React.FC = () => {
 
   const dispatch = useDispatch();
+  interface Review {
+    featured_photo: object
+  }
+
+  interface Reviews {
+    reviews: {
+      reviews: {
+        [key: number]: Review
+      }
+    }
+  }
+
   const reviews = useSelector((state: any) => state.reviews.reviews);
 
   useEffect(() => {
@@ -28,13 +43,35 @@ const ReviewPage: React.FC = () => {
     getReviews();
   }, []);
 
+
+  const handleReviewClick = (e: object, reviewId: number) => {
+
+  }
+
+  const reviewItems = Object.keys(reviews).map((key, i) => {
+    let review = reviews[key as keyof Review];
+    return (
+      <IonItem key={i} onClick={(e) => handleReviewClick(e, i) }>
+        <IonThumbnail  className="reviewPhotoThumbnail">
+          <IonImg src={review?.featured_photo?.photo} />
+        </IonThumbnail>
+        {review.restaurant.name}
+      </IonItem>
+    )
+  })
+
   return (
     <>
       <IonPage>
         <Header headertitle="Reviews" />
+        <IonContent>
+          <IonList>
+            {reviewItems}
+          </IonList>
+        </IonContent>
       </IonPage>
     </>
   );
 };
 
-export default ReviewPage;
+export default ReviewsPage;
