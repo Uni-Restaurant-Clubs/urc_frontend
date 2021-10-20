6ed977c2c48ef06ad1de453376547c85ceed472c
Monty Lennie <montylennie@gmail.com>
2021-10-20 10:48:27 -0500

    add all fields to application form

--- src/pages/contentCreators/applicationForm/index.tsx
+++ src/pages/contentCreators/applicationForm/index.tsx
@@ -1,5 +1,9 @@
 import {
   IonButton,
+  IonImg,
+  IonRow,
+  IonCol,
+  IonGrid,
   IonCard,
   IonCardHeader,
   IonCardTitle,
@@ -7,6 +11,8 @@ import {
   IonContent,
   IonHeader,
   IonInput,
+  IonCheckbox,
+  IonList,
   IonItem,
   IonTextarea,
   IonLabel,
@@ -38,10 +44,19 @@ const CreatorApplicationForm: React.FC = () => {
   const [firstNameError, setfirstNameError] = useState<errorHandling | any>(null);
   const [lastNameError, setLastNameError] = useState<errorHandling | any>(null);
   const [emailError, setEmailError] = useState<errorHandling | any>(null);
+  const [isWriterError, setIsWriterError] = useState<errorHandling | any>(null);
+  const [isPhotographerError, setIsPhotographerError] = useState<errorHandling | any>(null);
+  const [isVideographerError, setIsVideographerError] = useState<errorHandling | any>(null);
 
   const [firstName, setFirstName] = useState(null);
   const [lastName, setLastName] = useState(null);
   const [email, setEmail] = useState(null);
+  const [isWriter, setIsWriter] = useState(null);
+  const [isPhotographer, setIsPhotographer] = useState(null);
+  const [isVideographer, setIsVideographer] = useState(null);
+  const [introApplicationText, setIntroApplicationText] = useState(null);
+  const [experiencesApplicationText, setExperiencesApplicationText] = useState(null);
+  const [whyJoinApplicationText, setWhyJoinApplicationText] = useState(null);
 
   const [showAlert, setShowAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
@@ -66,6 +81,9 @@ const CreatorApplicationForm: React.FC = () => {
         firstName,
         lastName,
         email,
+        isWriter,
+        isPhotographer,
+        isVideographer
       },
       recaptchaToken
     };
@@ -116,107 +134,207 @@ const CreatorApplicationForm: React.FC = () => {
   }, [apiError]);
 
   return (
-    <div className=" ">
-      <IonPage>
-        <Header headertitle="Creator Application" />
-        <IonContent>
-          <IonCard className="applicationCard">
-            <IonCardContent>
-              <IonCardHeader>
-                <IonCardTitle>
-                  Application Form
-                </IonCardTitle>
-                <IonCardTitle>
-                  Writers/Photographers
-                </IonCardTitle>
-              </IonCardHeader>
-              <br/>
-              <p>Thank you for your interest!</p>
-              <p>Please fill out the following fields and submit the form.</p>
-              <p>We will review and reply to you shortly!</p>
-              <br/>
+    <IonPage>
+      <Header headertitle="Creator Application" />
+      <IonContent>
+        <IonCard className="applicationCard">
+          <IonCardContent>
+            <IonCardHeader>
+              <IonCardTitle>
+                Application Form
+              </IonCardTitle>
+              <IonCardTitle>
+                Writers/Photographers (Brooklyn)
+              </IonCardTitle>
+            </IonCardHeader>
+            <IonImg height="100" className="applicationShieldLogo" src="https://urc-public-images.s3.us-east-2.amazonaws.com/output-onlinepngtools+(1).png"/>
+            <br/>
+            <p>Thank you for your interest!</p>
+            <p>Please fill out the following fields and submit the form.</p>
+            <p>We will review and reply to you shortly!</p>
+            <br/>
+            <IonImg src="https://urc-public-images.s3.us-east-2.amazonaws.com/photo-1592861956120-e524fc739696.jpeg"/>
+            <br/>
 
-              <IonLoading
-                spinner="bubbles"
-                message="Please wait ..."
-                duration={0}
-                isOpen={loading}
-              />
+            <IonLoading
+              spinner="bubbles"
+              message="Please wait ..."
+              duration={0}
+              isOpen={loading}
+            />
 
-              <IonAlert
-                isOpen={showAlert}
-                onDidDismiss={() => {
-                  setShowAlert(false);
-                  setAlertMessage("");
-                }}
-                header={"Alert"}
-                message={alertMessage}
-                buttons={[
-                  {
-                    text: "Ok",
-                    cssClass: "confirmButtonStyle rightButton",
-                    handler: () => {
-                      setAlertMessage("");
-                    },
+            <IonAlert
+              isOpen={showAlert}
+              onDidDismiss={() => {
+                setShowAlert(false);
+                setAlertMessage("");
+              }}
+              header={"Alert"}
+              message={alertMessage}
+              buttons={[
+                {
+                  text: "Ok",
+                  cssClass: "confirmButtonStyle rightButton",
+                  handler: () => {
+                    setAlertMessage("");
                   },
-                ]}
-              />
+                },
+              ]}
+            />
 
-              <div className="">
+            <p className="applyRequiredText">* Required</p>
+            <IonGrid>
+              <IonRow>
+                <IonCol sizeSm="12" sizeMd="6">
+                  <IonItem>
+                    <IonLabel
+                      color={firstNameError ? "danger" : ""}
+                      position="floating"
+                    >
+                      First Name *
+                    </IonLabel>
+                    <IonInput
+                      placeholder="First Name"
+                      value={firstName}
+                      onIonChange={(e: any) => setFirstName(e.target.value)}
+                    />
+                  </IonItem>
+                </IonCol>
+                <IonCol sizeSm="12" sizeMd="6">
+                  <IonItem>
+                    <IonLabel
+                      color={lastNameError ? "danger" : ""}
+                      position="floating"
+                    >
+                      Last Name *
+                    </IonLabel>
+                    <IonInput
+                      placeholder="Last Name"
+                      value={lastName}
+                      onIonChange={(e: any) => setLastName(e.target.value)}
+                    />
+                  </IonItem>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol sizeSm="12" sizeMd="6">
+                  <IonItem>
+                    <IonLabel
+                      color={emailError ? "danger" : ""}
+                      position="floating"
+                    >
+                      Email address *
+                    </IonLabel>
+                    <IonInput
+                      placeholder="Enter your email address"
+                      value={email}
+                      required={true}
+                      onIonChange={(e: any) => setEmail(e.target.value)}
+                    ></IonInput>
+                  </IonItem>
+                </IonCol>
+              </IonRow>
+              <br/>
+              <p className="positionOptionsTitle">
+                Select all positions you would like to apply for *
+              </p>
+              <IonRow>
+                <IonCol sizeSm="6" sizeMd="6">
+                  <IonItem>
+                    <IonLabel>Writer</IonLabel>
+                    <IonCheckbox slot="start" checked={isWriter}
+                      onIonChange={e => setIsWriter(e.detail.checked)} />
+                  </IonItem>
+                </IonCol>
+                <IonCol sizeSm="6" sizeMd="6">
+                  <IonItem>
+                    <IonLabel>Photographer</IonLabel>
+                    <IonCheckbox slot="start" checked={isPhotographer}
+                      onIonChange={e => setIsPhotographer(e.detail.checked)} />
+                  </IonItem>
+                </IonCol>
+                <IonCol sizeSm="6" sizeMd="6">
+                  <IonItem>
+                    <IonLabel>Videographer</IonLabel>
+                    <IonCheckbox slot= "start" checked={isVideographer}
+                        onIonChange={e => setIsVideographer(e.detail.checked)} />
+                  </IonItem>
+                </IonCol>
+              </IonRow>
+              <br/>
+              <IonItem>
+                <IonLabel position="floating">Tell us about yourself *</IonLabel>
+                <IonTextarea className="applicationTextArea" value={introApplicationText}
+                    autoGrow="true"
+                    onIonChange={e => setIntroApplicationText(e.detail.value!)}>
+                </IonTextarea>
+              </IonItem>
+              <IonItem>
+                <IonLabel position="floating">What experience do you have? *</IonLabel>
+                <IonTextarea className="applicationTextArea" value={experiencesApplicationText}
+                    autoGrow="true"
+                    onIonChange={e => setExperiencesApplicationText(e.detail.value!)}>
+                </IonTextarea>
+              </IonItem>
+              <IonItem>
+                <IonLabel position="floating">Why do you want to join? *</IonLabel>
+                <IonTextarea className="applicationTextArea" value={whyJoinApplicationText}
+                    autoGrow="true"
+                    onIonChange={e => setWhyJoinApplicationText(e.detail.value!)}>
+                </IonTextarea>
+              </IonItem>
+              <br/>
+              <br/>
+              <IonItem>
+                <IonLabel position="stacked">Social media/website links *</IonLabel>
+                <IonInput value={whyJoinApplicationText}
+                    placeholder="https://instagram.com/me, https://mywebsite.com etc..."
+                    onIonChange={e => setWhyJoinApplicationText(e.detail.value!)}>
+                </IonInput>
+              </IonItem>
+              <br/>
+              <IonItem>
+                <IonLabel
+                  color={firstNameError ? "danger" : ""}
+                  position="stacked"
+                >
+                Resume
+                </IonLabel>
+                <IonInput
+                  type="file"
+                  value={firstName}
+                  onIonChange={(e: any) => setFirstName(e.target.value)}
+                />
+              </IonItem>
+              <br/>
+              { isWriter &&
                 <IonItem>
                   <IonLabel
                     color={firstNameError ? "danger" : ""}
-                    position="floating"
+                    position="stacked"
                   >
-                    First Name
+                  Writing Example *
                   </IonLabel>
                   <IonInput
-                    placeholder="First Name"
+                    type="file"
                     value={firstName}
                     onIonChange={(e: any) => setFirstName(e.target.value)}
                   />
                 </IonItem>
-                <IonItem>
-                  <IonLabel
-                    color={lastNameError ? "danger" : ""}
-                    position="floating"
-                  >
-                    Last Name
-                  </IonLabel>
-                  <IonInput
-                    placeholder="Last Name"
-                    value={lastName}
-                    onIonChange={(e: any) => setLastName(e.target.value)}
-                  />
-                </IonItem>
-                <IonItem>
-                  <IonLabel
-                    color={emailError ? "danger" : ""}
-                    position="floating"
-                  >
-                    Email address
-                  </IonLabel>
-                  <IonInput
-                    placeholder="Enter your email address"
-                    value={email}
-                    required={true}
-                    onIonChange={(e: any) => setEmail(e.target.value)}
-                  ></IonInput>
-                </IonItem>
-                <IonButton
-                  expand="block"
-                  onClick={submitCreatorsApplication}
-                  style={{ marginTop: "1rem" }}
-                >
-                  Submit Application
-                </IonButton>
+              }
+              <IonButton
+                expand="block"
+                onClick={submitCreatorsApplication}
+                style={{ marginTop: "1rem" }}
+              >
+                Submit Application
+              </IonButton>
 
-              </div>
-            </IonCardContent>
-          </IonCard>
-        </IonContent>
-      </IonPage>
-    </div>
+            </IonGrid>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
   );
 };
 
