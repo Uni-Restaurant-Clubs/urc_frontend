324cd83c4db82f46831133d4ed9038f1082aa779
Monty Lennie <montylennie@gmail.com>
2021-12-06 17:31:58 -0500

    fix ad section

--- src/pages/reviews/details/index.tsx
+++ src/pages/reviews/details/index.tsx
@@ -38,6 +38,8 @@ const ReviewPage: React.FC = () => {
   }, [id]);
 
   const iframe = '<iframe f=ifr&lt1=_new src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=21&l=ur1&category=gourmet&banner=1HN0SQGXKCCJ4YE167G2&f=ifr<1=_new&linkID=69a3cd67d71f12cd5ad579d57e8f7e70&t=unirestaurant-20&tracking_id=unirestaurant-20" width="125" height="125" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>'
+  const iframe2 = '<iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=21&l=ur1&category=gourmet&banner=1F6R1JY1XW26C7B9K2G2&f=ifr&linkID=c3950f4803d1fc1ff372fb6da56edc08&t=unirestaurant-20&tracking_id=unirestaurant-20" width="125" height="125" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>'
+
   return (
     <>
       <IonPage>
@@ -57,9 +59,9 @@ const ReviewPage: React.FC = () => {
                 writer={review?.writer}
                 photographer={review?.photographer}
               />
-              <div dangerouslySetInnerHTML={ {__html: iframe} } />
-              <div className="googleAdContainer">
-                <Ad/>
+              <div className="amazonAdContainer">
+                <div className="amazonAd" dangerouslySetInnerHTML={ {__html: iframe} } />
+                <div dangerouslySetInnerHTML={ {__html: iframe2} } />
               </div>
               <br />
               <ReviewArticle title={review?.article_title} article={review?.article} />
